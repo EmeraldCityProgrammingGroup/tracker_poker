7d183ebc697e3008936dc32fc267375d64cbe6e5
Allan Davis <cajun.code@gmail.com>
2011-11-19 12:00:15 -0500

    Inital Project Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackerPoker::Application.config.secret_token = '7dd3ef5d2e9624f28242e785fd60750247514033b6fe4eaafdbf8d77a669de2ca55a67780811d55fab18cb1f0c0e31f6c7c93f7acb75e7e4e534556887642fab'
