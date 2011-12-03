2d32683c5e35f9f1785b71f2864a5e25f54c5547
Allan Davis <cajun.code@gmail.com>
2011-12-03 11:44:51 -0500

    configure devise

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,6 @@ module TrackerPoker
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
