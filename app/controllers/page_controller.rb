96c965596ffcfdeeb9f8d7a69895302d99b39887
Allan Davis <cajun.code@gmail.com>
2011-12-03 11:07:36 -0500

    Added 960 gtrid to app as well as title helper

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,11 +1,13 @@
 class PageController < ApplicationController
   def index
+    @title = "Home"
   end
 
   def about
   end
 
   def contact
+    @title = "Contact us"
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    if @title
+      result = "Tracker Poker - #{@title}"
+    else
+      result = "Tracker Poker"
+    end
+    result
+  end
 end
