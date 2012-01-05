a0aad90e26b267f08624c62e4324b26b1648369d
Allan Davis <cajun.code@gmail.com>
2012-01-05 09:08:07 -0500

    Got app listed unestimated stories

--- app/controllers/pivotal_controller.rb
+++ app/controllers/pivotal_controller.rb
@@ -1,14 +1,36 @@
 class PivotalController < ApplicationController
+  def index
+    unless current_user.pivotal_users.empty?
+      PivotalTracker::Client.token = current_user.pivotal_users.last.token
+      redirect_to :pivotal_projects
+    end
+  end
+  
   def login
+    username = params[:user_name]
+    password = params[:password]
+    PivotalTracker::Client.token = nil
+    token = PivotalTracker::Client.token(username, password)   
+    current_user.pivotal_users.create :token => token
+    unless token.nil?
+      redirect_to :pivotal_projects
+    else
+      redirect_to :povital
+    end
   end
 
   def projects
+    @projects = PivotalTracker::Project.all 
   end
   
   def stories 
 
     @project = PivotalTracker::Project.find(params[:project_id].to_i)
-    @stories = @project.stories.all :current_state=>"unscheduled"
+    # @stories = @project.stories.all( :current_state=>"unscheduled").map{|x| x if x.estimate == -1}.compact
+    @stories = @project.stories.all
+    @active_count = @stories.size
+    @stories = @stories.collect{|x| x if x.estimate == -1}.compact
+    @unestimated_count = @stories.size
   end
   
   
