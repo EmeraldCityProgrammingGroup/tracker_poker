ddffb55306f4f3903f427f8e9cb5839fd5f64a95
Allan Davis <cajun.code@gmail.com>
2012-01-15 23:55:42 -0500

    Switched to database session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-TrackerPoker::Application.config.session_store :cookie_store, key: '_tracker_poker_session'
+# TrackerPoker::Application.config.session_store :cookie_store, key: '_tracker_poker_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# TrackerPoker::Application.config.session_store :active_record_store
+TrackerPoker::Application.config.session_store :active_record_store
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111217170206) do
+ActiveRecord::Schema.define(:version => 20120116045223) do
 
   create_table "pivotal_users", :force => true do |t|
     t.integer  "user_id"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(:version => 20111217170206) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "story_votes", :force => true do |t|
     t.integer  "story_id"
