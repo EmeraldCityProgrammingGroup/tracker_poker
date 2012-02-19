0b1e4b22f7b044df5bc2ebd6e97fc399c5dba4c8
Allan Davis <cajun.code@gmail.com>
2012-02-19 17:59:29 -0500

    Added close button on room hub and set the play test as pending

--- spec/controllers/play_controller_spec.rb
+++ spec/controllers/play_controller_spec.rb
@@ -4,6 +4,7 @@ describe PlayController do
 
   describe "GET 'index'" do
     it "returns http success" do
+      pending "have not started the play controller yet"
       get 'index'
       response.should be_success
     end
